67f813ddb5fc24cb9cbb7ba92064b6f1eef427dc
currentversion-studios <33076385+currentversion-studios@users.noreply.github.com>
2017-11-14 11:20:06 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Nov 14, 2017 11:20:45").getTime();
+var countDownDate = new Date("Nov 14, 2017 11:30:45").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
